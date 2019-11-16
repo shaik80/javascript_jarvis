3c0b47ea3647c8f4b05d211b073d8bc2349f7108
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-16 00:50:53 -0800

    audio and image

--- js/app.js
+++ js/app.js
@@ -1,28 +1,56 @@
-// const btn = document.getElementById("btn");
-// const result = document.getElementById("result");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
 
-// const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-// const recognition = new SpeechRecognition();
-// console.log(recognition)
-// recognition.onstart = () => {
-//   console.log("recording started");
-// };
+const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+const recognition = new SpeechRecognition();
+console.log(recognition)
+recognition.onstart = () => {
+  console.log("recording started");
+};
 
-// recognition.onresult = e => {
-//     let text = e.results[0][0].transcript
-//   console.log(text);
-//   read(text)
-// };
+recognition.onresult = e => {
+    let text = e.results[0][0].transcript
+  console.log(text);
+  read(text)
+};
 
-// read = (text) =>{
-//     let speech = new SpeechSynthesisUtterance();
+read = (text) =>{
+    let speech = new SpeechSynthesisUtterance();
     
-//     if(text.includes("what's your name")){
-//         speech.text = "im jarvis" ;
-//     }
-//     else{
-//         speech.text = text;
-//     }
-//     window.speechSynthesis.speak(speech)
-// }
+    if(text.includes("what's your name")){
+        speech.text = "im jarvis" ;
+    }
+    else{
+        speech.text = text;
+    }
+    window.speechSynthesis.speak(speech)
+}
 
+
+        
+(async () => {
+  const model = await mobilenet.load()
+  const status = document.getElementById('status');
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext("2d");
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+          facingMode: 'environment'
+      }
+  })
+
+  // Stream  -> raw stream of video
+  video.srcObject = stream
+  
+  predict()
+
+  async function predict() {
+      ctx.drawImage(video, 0, 0, 500, 500)
+      const predictions = await model.classify(canvas)
+      status.innerHTML = `Prediction:  ${predictions[0].className}  /  ${predictions[0]}`
+      requestAnimationFrame(predict)
+  }
+})()
